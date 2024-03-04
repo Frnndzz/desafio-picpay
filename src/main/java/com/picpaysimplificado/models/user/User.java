a47eb7577d265b345ccd119531920fde85faaacb
Gustavo Fernandez <gustavoffontes07@gmail.com>
2024-03-04 20:12:37 -0300

    feat: create User entity

---
+++ src/main/java/com/picpaysimplificado/models/user/User.java
@@ -0,0 +1,36 @@
+package com.picpaysimplificado.models.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
