7206fbc22a964f08d58bd9ccd5af8df6eed75a8a
Gustavo Fernandez <gustavoffontes07@gmail.com>
2024-03-05 11:02:19 -0300

    feat: create new constructor in the User entity

--- src/main/java/com/picpaysimplificado/models/user/User.java
+++ src/main/java/com/picpaysimplificado/models/user/User.java
@@ -1,5 +1,6 @@
 package com.picpaysimplificado.models.user;
 
+import com.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -16,8 +17,8 @@ import java.util.UUID;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String firstName;
     private String lastName;
@@ -33,4 +34,15 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.document = data.document();
+        this.email = data.email();
+        this.password = data.password();
+        this.balance = data.balance();
+        this.userType = data.userType();
+    }
+
 }
